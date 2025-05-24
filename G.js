8ee8b4d303b41fbbf3a3adf2fadbea901208ce53
umashankargi <108452609+umashankargi@users.noreply.github.com>
2025-05-24 18:34:40 +0530

    G.js

--- G.js
+++ G.js
@@ -89,7 +89,7 @@ document.querySelectorAll('.whatsapp-float').forEach(btn => {
 document.querySelectorAll('.whatsapp-community-alert').forEach(btn => {
     btn.addEventListener('click', function(e) {
         e.preventDefault();
-        window.open('https://chat.whatsapp.com/CJ2YjvRefRcGIU5W7qtbTg', '_blank');
+        window.open('https://chat.whatsapp.com/DUi8XW13jBo66VkSoMezUJ', '_blank');
     });
 });
 
